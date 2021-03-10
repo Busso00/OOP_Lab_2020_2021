9f7e03ca245da0e455b7e82e9ab95202150bb1e1
s269282 <s269282@eba5c24d-6beb-4af8-bc34-13728630496d>
2021-03-10 16:47:47 +0000

    New Package

---
+++ ProgettoCommit/src/Pacchetto1/prova1.java
@@ -0,0 +1,9 @@
+package Pacchetto1;
+
+public class prova1 {
+	static int i=5;
+	
+	public static void stampa(){
+		System.out.println("la variabile è: "+i);	
+	}
+}
--- ProgettoCommit/src/Prova.java
+++ ProgettoCommit/src/Prova.java
@@ -1,7 +1,9 @@
+import Pacchetto1.prova1;
 
 public class Prova {
 	public static void main(String[] args) {
 		System.out.println("ciao");
 		System.out.println("ciao2");
+		Pacchetto1.prova1.stampa();
 	}
 }
